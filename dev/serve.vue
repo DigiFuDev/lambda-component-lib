17d24d63b9878f3b4f2ce630e5fa024f06717fa0
Victor V <victorvaldez5@gmail.com>
2021-10-12 11:22:56 -0400

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { LambdaComponentLibSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  LambdaComponentLibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <lambda-component-lib-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as LambdaComponentLibSample } from './lambda-component-lib-sample.vue';
